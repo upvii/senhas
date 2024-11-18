5dc129225049fda65e3ed6314d5ad20a3b38685b
vii pieri <128058367+upvii@users.noreply.github.com>
2024-11-18 11:47:58 -0300

    Add files via upload

--- Vídeos/main.js
+++ Vídeos/main.js
@@ -1,5 +1,19 @@
 const numeroSenha= document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=12;
 numeroSenha.textContent= tamanhoSenha;
-const botoes= document.querySelector('.parametro-senha-botoes');
+const botoes= document.querySelectorAll('.parametro-senha-botao');
 botoes[0].onclick= diminuirTamanho;
+function diminuirTamanho(){
+    if(tamanhoSenha>7){
+    tamanhoSenha= tamanhoSenha-1;
+    numeroSenha.textContent= tamanhoSenha;
+}
+}
+botoes[1].onclick= aumentarTamanho;
+
+function aumentarTamanho(){
+    if(tamanhoSenha<32){
+    tamanhoSenha= tamanhoSenha+1;
+    numeroSenha.textContent= tamanhoSenha;
+}
+}
