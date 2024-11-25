2d3a81e1645ef7f63480276bc87136b6f4a2090c
vii pieri <128058367+upvii@users.noreply.github.com>
2024-11-25 11:44:37 -0300

    Add files via upload

--- Vídeos/main.js
+++ Vídeos/main.js
@@ -1,7 +1,9 @@
 const numeroSenha= document.querySelector('.parametro-senha-texto');
-let tamanhoSenha=12;
+let tamanhoSenha=32;
 numeroSenha.textContent= tamanhoSenha;
 const botoes= document.querySelectorAll('.parametro-senha-botao');
+const campoSenha= document.querySelector("#campo-senha");
+const letrasMaiusculas= "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ";
 botoes[0].onclick= diminuirTamanho;
 function diminuirTamanho(){
     if(tamanhoSenha>7){
@@ -10,10 +12,20 @@ function diminuirTamanho(){
 }
 }
 botoes[1].onclick= aumentarTamanho;
+geraSenha();
 
 function aumentarTamanho(){
     if(tamanhoSenha<32){
     tamanhoSenha= tamanhoSenha+1;
     numeroSenha.textContent= tamanhoSenha;
 }
+}
+function geraSenha(){
+    let senha=" ";
+    for(let i=0; i<tamanhoSenha;i++){
+    let numeroAleatorio= Math.random()*letrasMaiusculas.length;
+    numeroAleatorio= Math.floor(numeroAleatorio);
+    senha=senha+letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value=senha;
 }
